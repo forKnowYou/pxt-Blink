035b41ef80d787aa36588a443565925bfbb59ad1
forKnowYou <1003425554@qq.com>
2019-04-04 15:49:13 +0800

    make

---
+++ main.ts
@@ -0,0 +1,24 @@
+
+enum ePin_t {
+    //% block="PIN0"
+    PIN0,
+    //% block="PIN1"
+    PIN1
+}
+
+enum eLevel_t {
+    //% block="LOW"
+    LOW,
+    //% block="HIGH"
+    HIGH
+}
+
+//% color=#ff0000 icon=""
+namespace Blink {
+
+    //% block
+    export function setLevel(ePin: ePin_t, eLevel: eLevel_t): void {
+        
+    }
+
+}
